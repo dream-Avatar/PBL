6880a5c2ea6712059c3207bfeeff756db0e4dbbe
Bobzgamez <123031496+dream-Avatar@users.noreply.github.com>
2024-03-03 15:05:32 -0600

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -109,7 +109,7 @@ var games = {
     "warioland4":"Wario Land 4",
     "wario_ware":"Wario Ware Inc",
     "zelda_past":"The Legend of Zelda: A Link to the Past",
-    "zelda_minish":"The Legend of Zelda: The Minish Cap"
+    "zelda_minish":"The Legend of Zelda: The Minish Cap",
     "onepeice" : "One Piece"
 };
 var Iodine = null;
